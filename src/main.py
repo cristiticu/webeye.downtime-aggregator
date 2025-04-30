534595f89a4b9bee545eef3b583f48da6c62071f
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-30 18:13:11 +0300

    Add threading of sqs events

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
+from concurrent.futures import ThreadPoolExecutor
 import itertools
+import json
 from typing import Any, TYPE_CHECKING
 
 from context import ApplicationContext
-import settings
+
 if TYPE_CHECKING:
     from aws_lambda_typing.context import Context
+    from aws_lambda_typing.events import SQSEvent
 
 
 application_context = ApplicationContext()
 
 
-def lambda_handler(event: dict[str, Any], context: "Context") -> dict[str, Any]:
-    u_guid = event["u_guid"]
-    url = event["url"]
+def process_record(aggregate_request) -> dict[str, Any]:
+    print(f"Processing record {aggregate_request}")
 
     try:
+        u_guid: str = aggregate_request["u_guid"]
+        configuration = aggregate_request["configuration"]
+
+        url: str = configuration["url"]
+
         scheduled_checks = application_context.scheduled_tasks_persistence.get_all_scheduled_checks(
             u_guid, url)
 
@@ -43,7 +50,15 @@ def lambda_handler(event: dict[str, Any], context: "Context") -> dict[str, Any]:
         }
 
 
-if __name__ == "__main__":
-    if settings.ENVIRONMENT == "dev":
-        application_context.events.check_for_downtime(
-            "fb8b90bc-6015-4b46-8e6a-c2bb178d97f6", "https://weather.cristit.icu", ['america'])
+def lambda_handler(event: "SQSEvent", context: "Context"):
+    records = event.get("Records")
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(process_record, [json.loads(record.get(
+            "body", "")) for record in records], timeout=20)
+
+
+# if __name__ == "__main__":
+#     if settings.ENVIRONMENT == "test":
+#         application_context.events.check_for_downtime(
+#             "fb8b90bc-6015-4b46-8e6a-c2bb178d97f6", "https://weather.cristit.icu", ['america'])
